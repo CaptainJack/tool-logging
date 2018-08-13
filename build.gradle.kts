5fb745a2503ff3f58a24adebd31faecda1cc5ca1
Alexey Chuprin <a.chuprin@capjack.ru>
2018-08-13 11:32:05 +0300

    Update ru.capjack.kotlin-sources-jar 0.3.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
 	kotlin("jvm") version "1.2.60" apply false
 	id("nebula.release") version "6.3.5"
-	id("ru.capjack.kotlin-sources-jar") version "0.2.0" apply false
+	id("ru.capjack.kotlin-sources-jar") version "0.3.0" apply false
 	id("ru.capjack.capjack-publish") version "0.5.0" apply false
 }
 
