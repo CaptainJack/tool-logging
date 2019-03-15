19a295625847ec1c8343433844f064654badae22
Alexey Chuprin <a.chuprin@capjack.ru>
2019-03-15 13:54:41 +0300

    Full support package name resolving of logging classes in js

--- gradle-plugin/src/main/kotlin/ru/capjack/tool/kt/logging/gradle/JsExtension.kt
+++ gradle-plugin/src/main/kotlin/ru/capjack/tool/kt/logging/gradle/JsExtension.kt
@@ -1,6 +1,8 @@
 package ru.capjack.tool.kt.logging.gradle
 
 import org.jetbrains.kotlin.descriptors.ClassDescriptor
+import org.jetbrains.kotlin.descriptors.resolveClassByFqName
+import org.jetbrains.kotlin.incremental.components.NoLookupLocation
 import org.jetbrains.kotlin.js.backend.ast.JsNameRef
 import org.jetbrains.kotlin.js.backend.ast.JsStringLiteral
 import org.jetbrains.kotlin.js.translate.context.Namer
@@ -11,81 +13,66 @@ import org.jetbrains.kotlin.js.translate.utils.BindingUtils
 import org.jetbrains.kotlin.js.translate.utils.JsAstUtils
 import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.psi.KtCallExpression
-import org.jetbrains.kotlin.psi.KtDotQualifiedExpression
-import org.jetbrains.kotlin.psi.KtProperty
 import org.jetbrains.kotlin.psi.KtPureClassOrObject
 import org.jetbrains.kotlin.psi.KtReferenceExpression
-import org.jetbrains.kotlin.resolve.calls.callUtil.getCall
+import org.jetbrains.kotlin.psi.KtTreeVisitor
+import org.jetbrains.kotlin.resolve.calls.callUtil.getType
 import org.jetbrains.kotlin.resolve.descriptorUtil.fqNameSafe
 import org.jetbrains.kotlin.types.KotlinType
 
 class JsExtension : JsSyntheticTranslateExtension {
 	
 	private var fqNameLogger = FqName("ru.capjack.tool.kt.logging.Logger")
-	private var fqNameLogging = FqName("ru.capjack.tool.kt.logging.Logging")
-	
 	private var fqNameGetLogger = FqName("ru.capjack.tool.kt.logging.Logging.getLogger")
 	private var fqNameGetLoggerExt = FqName("ru.capjack.tool.kt.logging.getLogger")
 	
+	private val processedTypes = mutableSetOf<KotlinType>()
 	
 	override fun generateClassSyntheticParts(declaration: KtPureClassOrObject, descriptor: ClassDescriptor, translator: DeclarationBodyVisitor, context: TranslationContext) {
-		declaration.declarations
-			.filterIsInstance<KtProperty>()
-			.forEach { processProperty(context, it) }
 		
-	}
-	
-	private fun processProperty(context: TranslationContext, property: KtProperty) {
-		val bindingContext = context.bindingContext()
-		
-		if (BindingUtils.getPropertyDescriptor(bindingContext, property).type.constructor.declarationDescriptor?.fqNameSafe != fqNameLogger) {
-			return
-		}
-		
-		val initializer = property.initializer as? KtDotQualifiedExpression
+		val loggerType = context.currentModule.resolveClassByFqName(fqNameLogger, NoLookupLocation.FROM_BACKEND)?.defaultType
 			?: return
 		
-		val target = initializer.firstChild as? KtReferenceExpression
-			?: return
-		
-		if (BindingUtils.getDescriptorForReferenceExpression(bindingContext, target)?.fqNameSafe != fqNameLogging) {
-			return
-		}
-		
-		val call = (initializer.lastChild as? KtCallExpression)?.getCall(bindingContext)
-			?: return
-		
-		val calleeExpression = call.calleeExpression as? KtReferenceExpression
-			?: return
-		
-		val fnName = BindingUtils.getDescriptorForReferenceExpression(bindingContext, calleeExpression)?.fqNameSafe
-			?: return
-		
-		val type = when (fnName) {
-			fqNameGetLogger    -> {
-				if (call.valueArguments.size != 1) {
-					return
-				}
-				BindingUtils.getTypeForExpression(bindingContext, call.valueArguments[0].getArgumentExpression() ?: return).arguments.let {
-					if (it.size != 1) return
-					it[0].type
-				}
-			}
-			fqNameGetLoggerExt -> {
-				if (call.typeArguments.size != 1) {
-					return
+		val visitor = object : KtTreeVisitor<Unit>() {
+			override fun visitCallExpression(expression: KtCallExpression, data: Unit): Void? {
+				
+				val type = (expression.calleeExpression as? KtReferenceExpression)?.let { referenceExpression ->
+					val bindingContext = context.bindingContext()
+					
+					if (expression.getType(bindingContext) == loggerType) {
+						
+						when (BindingUtils.getDescriptorForReferenceExpression(bindingContext, referenceExpression)?.fqNameSafe) {
+							fqNameGetLogger    -> {
+								if (expression.valueArguments.size == 1 && expression.typeArguments.isEmpty()) {
+									BindingUtils.getTypeForExpression(bindingContext, expression.valueArguments[0].getArgumentExpression()!!).arguments[0].type
+								}
+								else null
+							}
+							fqNameGetLoggerExt -> {
+								if (expression.valueArguments.isEmpty() && expression.typeArguments.size == 1) {
+									BindingUtils.getTypeByReference(bindingContext, expression.typeArguments[0].typeReference!!)
+								}
+								else null
+							}
+							else               -> null
+						}
+					}
+					else null
 				}
 				
-				BindingUtils.getTypeByReference(bindingContext, call.typeArguments[0].typeReference ?: return)
+				if (type != null) {
+					processType(context, type)
+				}
+				
+				return super.visitCallExpression(expression, data)
 			}
-			else               -> return
 		}
 		
-		processType(context, type)
 		
+		declaration.declarations.forEach {
+			it.accept(visitor, Unit)
+		}
 	}
-	
-	private val processedTypes = mutableSetOf<KotlinType>()
 	
 	private fun processType(context: TranslationContext, type: KotlinType) {
 		
