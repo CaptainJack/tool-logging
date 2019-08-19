4b9fcf26cf8c5440d81506e6a157ad912122bd05
Alexey Chuprin <a.chuprin@capjack.ru>
2019-08-19 16:58:59 +0300

    gradle-depver:0.2.0

--- gradle-plugin/build.gradle.kts
+++ gradle-plugin/build.gradle.kts
@@ -14,7 +14,7 @@ dependencies {
 	compileOnly(kotlin("stdlib-jdk8"))
 	compileOnly(kotlin("gradle-plugin"))
 	compileOnly(kotlin("compiler-embeddable"))
-	compileOnly("ru.capjack.gradle:gradle-depver:0.1.0")
+	compileOnly("ru.capjack.gradle:gradle-depver:0.2.0")
 }
 
 gradlePlugin {
