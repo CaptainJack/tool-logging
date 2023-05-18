654f2202c0942aa0f32d9701322b88555a959565
Alexey Chuprin <a.chuprin@capjack.ru>
2023-05-18 19:32:41 +0300

    Kotlin 1.8.21 Gradle 8.1.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-	kotlin("multiplatform") version "1.7.10"
-	id("ru.capjack.publisher") version "1.0.0"
+	kotlin("multiplatform") version "1.8.21"
+	id("ru.capjack.publisher") version "1.1.0"
 }
 
 group = "ru.capjack.tool"
@@ -12,7 +12,7 @@ repositories {
 
 kotlin {
 	jvm {
-		compilations.all { kotlinOptions.jvmTarget = "17" }
+		jvmToolchain(17)
 	}
 	js(IR) {
 		browser()
@@ -24,14 +24,14 @@ kotlin {
 		}
 		
 		get("jvmMain").dependencies {
-			implementation("org.slf4j:slf4j-api:1.7.36")
+			implementation("org.slf4j:slf4j-api:2.0.7")
 		}
 		get("jvmTest").dependencies {
-			implementation("ch.qos.logback:logback-classic:1.2.11")
+			implementation("ch.qos.logback:logback-classic:1.4.7")
 		}
 		
 		get("jsMain").dependencies {
-			implementation("ru.capjack.tool:tool-lang:1.13.1")
+			implementation("ru.capjack.tool:tool-lang:1.14.0")
 		}
 	}
 }
